342b8eedbd626ecd761c64ad8f92f0cd860f79bd
Alexey <alex008145@gmail.com>
2025-10-31 23:04:25 +0300

    30.10

---
+++ JavaScript Complete/17 HTTP requests/assets/scripts/app.js
@@ -0,0 +1,67 @@
+const listElement = document.querySelector(".posts");
+const postTemplate = document.querySelector("#single-post");
+const form = document.querySelector("#new-post form");
+const fetchButton = document.querySelector("#available-posts button");
+
+function sendHttpRequest(method, url, data = null) {
+  const promise = new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+
+    xhr.open(method, url);
+
+    xhr.responseType = "json";
+
+    xhr.addEventListener("load", () => {
+      resolve(xhr.response);
+      // const response = JSON.parse(xhr.response);
+    });
+
+    xhr.send(JSON.stringify(data));
+  });
+
+  return promise;
+}
+
+async function fetchPosts() {
+  const responseData = await sendHttpRequest(
+    "GET",
+    "https://jsonplaceholder.typicode.com/posts"
+  );
+  const listOfPosts = responseData;
+  for (const post of listOfPosts) {
+    const postEl = document.importNode(postTemplate.content, true);
+    postEl.querySelector("h2").textContent = post.title.toUpperCase();
+    postEl.querySelector("p").textContent = post.body;
+    listElement.append(postEl);
+  }
+}
+
+async function createPost(title, content) {
+  const userId = Math.random() * 1000;
+  const post = {
+    title: title,
+    body: content,
+    userId: userId,
+  };
+
+  sendHttpRequest("POST", "https://jsonplaceholder.typicode.com/posts", post);
+}
+
+fetchButton.addEventListener("click", () => {
+  fetchPosts();
+  console.log("Posts fetched!");
+});
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const enteredTitle = event.currentTarget.querySelector("#title").value;
+  const enteredContent = event.currentTarget.querySelector("#content").value;
+
+  if (enteredTitle && enteredContent) {
+    createPost(enteredTitle, enteredContent);
+    event.currentTarget.reset();
+  } else {
+    alert("Please enter valid data!");
+  }
+});
+// createPost("Title", "Content");
